4cb7c3bcd58e7835300a76b61c9eb86777466353
Mystery0 <mystery0dyl520@gmail.com>
2021-09-20 18:04:48 +0800

    Init Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+fun String.runCommand(workingDir: File = file("./")): String {
+    val parts = this.split("\\s".toRegex())
+    val proc = ProcessBuilder(*parts.toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    proc.waitFor(1, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText().trim()
+}
+
+val gitVersionCode: Int = "git rev-list HEAD --count".runCommand().toInt()
+val gitVersionName = "git rev-parse --short=8 HEAD".runCommand()
+
+android {
+    compileSdk = 31
+    buildToolsVersion = "31.0.0"
+
+    defaultConfig {
+        applicationId = "vip.mystery0.xhu.timetable"
+        minSdk = 26
+        targetSdk = 31
+        versionCode = gitVersionCode
+        versionName = "1.0.0-alpha1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            versionNameSuffix = ".d$gitVersionCode.$gitVersionName"
+        }
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            versionNameSuffix = ".r$gitVersionCode.$gitVersionName"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerVersion = "1.5.21"
+    }
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+    signingConfigs {
+        create("release") {
+            storeFile = file(SignConfig.signKeyStoreFile)
+            storePassword = SignConfig.signKeyStorePassword
+            keyAlias = SignConfig.signKeyAlias
+            keyPassword = SignConfig.signKeyPassword
+        }
+    }
+}
+
+dependencies {
+    val composeVersion = rootProject.extra["compose_version"]
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+}
