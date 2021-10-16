f9585176b3b37c95dc63239f7aae3d6a61145b0d
Mystery0 <mystery0dyl520@gmail.com>
2021-10-17 02:15:42 +0800

    添加AppCenter构建脚本

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-mv build.gradle.kts build.gradle.kts.1
-sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' build.gradle.kts.1 > build.gradle.kts
-mv app/build.gradle.kts app/build.gradle.kts.1
-sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' app/build.gradle.kts.1 > app/build.gradle.kts
-mv buildSrc/build.gradle.kts buildSrc/build.gradle.kts.1
-sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' buildSrc/build.gradle.kts.1 > buildSrc/build.gradle.kts
-mv settings.gradle.kts settings.gradle.kts.1
-sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' settings.gradle.kts.1 > settings.gradle.kts
+
+function handle(){
+    input="$1"
+    mv "$APPCENTER_SOURCE_DIRECTORY/$input" "$APPCENTER_SOURCE_DIRECTORY/$input.1"
+    sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' "$APPCENTER_SOURCE_DIRECTORY/$input.1" > "$APPCENTER_SOURCE_DIRECTORY/$input"
+}
+
+handle "build.gradle.kts"
+handle "app/build.gradle.kts"
+handle "buildSrc/build.gradle.kts"
+handle "settings.gradle.kts"
