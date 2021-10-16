e5ad78d01f9c9a6a970f758a17f734c2ce89caaf
Mystery0 <mystery0dyl520@gmail.com>
2021-10-16 17:58:55 +0800

    添加调试界面和选项

--- app/src/main/java/vip/mystery0/xhu/timetable/ApplicationExt.kt
+++ app/src/main/java/vip/mystery0/xhu/timetable/ApplicationExt.kt
@@ -1,15 +1,24 @@
 package vip.mystery0.xhu.timetable
 
 import android.annotation.SuppressLint
+import android.app.Application
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.Settings
+import android.util.Log
 import androidx.browser.customtabs.CustomTabsIntent
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.AbstractCrashesListener
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.crashes.model.ErrorReport
 import vip.mystery0.xhu.timetable.base.BaseComposeActivity
+import vip.mystery0.xhu.timetable.config.GlobalConfig
 import java.io.File
 
 @SuppressLint("StaticFieldLeak")
@@ -44,6 +53,7 @@ fun isOnline(): Boolean {
 
 val externalPictureDir: File by lazy { context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!! }
 val externalDownloadDir: File by lazy { context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!! }
+val externalDocumentsDir: File by lazy { context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)!! }
 
 fun BaseComposeActivity.toCustomTabs(url: String) {
     try {
@@ -64,4 +74,50 @@ fun BaseComposeActivity.loadInBrowser(url: String) {
     } catch (e: ActivityNotFoundException) {
         "请先安装一个浏览器。".toast(true)
     }
+}
+
+fun registerAppCenter(application: Application) {
+    if (!AppCenter.isConfigured() && GlobalConfig.allowSendCrashReport) {
+        if (BuildConfig.DEBUG) {
+            AppCenter.setLogLevel(Log.VERBOSE)
+        }
+        Crashes.setListener(object : AbstractCrashesListener() {
+            override fun onBeforeSending(report: ErrorReport?) {
+                if (report == null) {
+                    return
+                }
+                try {
+                    dumpStackTraceToFile(report.appStartTime.time, report.stackTrace)
+                    Log.d(packageName, "onBeforeSending: write exception to file success")
+                } catch (e: Exception) {
+                    Log.w(packageName, "onBeforeSending: dump stackTrace failed", e)
+                }
+            }
+        })
+        AppCenter.setUserId(publicDeviceId)
+        AppCenter.start(
+            application,
+            "463eb268-c114-49de-880c-c596cf5be561",
+            Analytics::class.java,
+            Crashes::class.java
+        )
+    }
+}
+
+private fun dumpStackTraceToFile(timestamp: Long, stackTrace: String) {
+    val dumpDir = File(externalDocumentsDir, "crash")
+    val dumpFile = File(dumpDir, "Crash-${timestamp}.txt")
+    if (!dumpDir.exists()) dumpDir.mkdirs()
+    val content = buildString {
+        appendLine("===================================")
+        appendLine("应用版本: $appVersionName")
+        appendLine("Android版本: ${Build.VERSION.RELEASE}_${Build.VERSION.SDK_INT}")
+        appendLine("厂商: ${Build.MANUFACTURER}")
+        appendLine("型号: ${Build.MODEL}")
+        appendLine("设备id: $publicDeviceId")
+        appendLine("===================================")
+        appendLine()
+        appendLine(stackTrace)
+    }
+    dumpFile.writeText(content)
 }
