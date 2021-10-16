6fc677841ae12737bcfc9b971c5ff226f3c383af
Mystery0 <mystery0dyl520@gmail.com>
2021-10-17 02:05:58 +0800

    添加AppCenter构建脚本

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-sed -i 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' build.gradle.kts
-sed -i 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' app/build.gradle.kts
-sed -i 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' buildSrc/build.gradle.kts
-sed -i 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' settings.gradle.kts
+mv build.gradle.kts build.gradle.kts.1
+sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' build.gradle.kts.1 > build.gradle.kts
+mv app/build.gradle.kts app/build.gradle.kts.1
+sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' app/build.gradle.kts.1 > app/build.gradle.kts
+mv buildSrc/build.gradle.kts buildSrc/build.gradle.kts.1
+sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' buildSrc/build.gradle.kts.1 > buildSrc/build.gradle.kts
+mv settings.gradle.kts settings.gradle.kts.1
+sed 's|maven("https://nexus3.mystery0.vip/repository/maven-public/")||g' settings.gradle.kts.1 > settings.gradle.kts
