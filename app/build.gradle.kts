ae4cea522f44cadbc066f00336bafa39437f07a2
Mystery0 <mystery0dyl520@gmail.com>
2021-11-08 00:53:41 +0800

    release: 1.0.0-alpha3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 31
         versionCode = gitVersionCode
-        versionName = "1.0.0-alpha2"
+        versionName = "1.0.0-alpha3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
