53e21cbff166186f0b65c8aef54e01fefaf098cd
Mystery0 <mystery0dyl520@gmail.com>
2022-02-12 18:46:18 +0800

    release: 1.0.0-beta3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 31
         versionCode = gitVersionCode
-        versionName = "1.0.0-beta2"
+        versionName = "1.0.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
