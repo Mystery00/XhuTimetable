034ce2e0a60d847e79f1c1b0695886d5f24bd6d7
Mystery0 <mystery0dyl520@gmail.com>
2022-03-06 18:44:33 +0800

    fix: 修复启动页面显示问题

---
+++ app/src/main/java/vip/mystery0/xhu/timetable/ui/activity/SplashImageActivity.kt
@@ -0,0 +1,106 @@
+package vip.mystery0.xhu.timetable.ui.activity
+
+import android.os.Build
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsPadding
+import vip.mystery0.xhu.timetable.base.BaseComposeActivity
+import vip.mystery0.xhu.timetable.config.DataHolder
+import vip.mystery0.xhu.timetable.ui.theme.XhuTimetableTheme
+import vip.mystery0.xhu.timetable.viewmodel.SplashImageViewModel
+
+class SplashImageActivity : BaseComposeActivity() {
+    private val viewModel: SplashImageViewModel by viewModels()
+    private val loader: ImageLoader by lazy {
+        ImageLoader(this).newBuilder()
+            .components {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
+    }
+
+    @Composable
+    override fun BuildContentWindow() {
+        XhuTimetableTheme {
+            ProvideWindowInsets {
+                val timer by viewModel.timerState.collectAsState()
+                val showSplash = DataHolder.splashFile
+                val backgroundColor = DataHolder.backgroundColor
+                Box(
+                    modifier = Modifier
+                        .background(backgroundColor ?: MaterialTheme.colors.background)
+                        .fillMaxSize()
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(showSplash)
+                            .crossfade(true)
+                            .diskCachePolicy(CachePolicy.DISABLED)
+                            .build(),
+                        imageLoader = loader,
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .fillMaxSize(),
+                    )
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(36.dp)
+                            .navigationBarsPadding()
+                            .background(Color(0x80000000), shape = RoundedCornerShape(24.dp))
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null,
+                                onClick = {
+                                    viewModel.skip()
+                                }
+                            )) {
+                        Text(
+                            modifier = Modifier.padding(12.dp),
+                            text = "跳 过 $timer",
+                            fontSize = 12.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+                if (timer <= 0) {
+                    intentTo(MainActivity::class)
+                    finish()
+                }
+            }
+        }
+    }
+}
