c609b7176e470b827d1ce68010ed5ac62d069a22
Mystery0 <mystery0dyl520@gmail.com>
2022-04-05 18:37:31 +0800

    feat: 校历支持分享到其他应用

---
+++ app/src/main/java/vip/mystery0/xhu/timetable/ui/theme/Font.kt
@@ -0,0 +1,8 @@
+package vip.mystery0.xhu.timetable.ui.theme
+
+object XhuFonts {
+//    @OptIn(ExperimentalTextApi::class)
+//    val fuMengTi = FontFamily(
+//        Font(File(externalDocumentsDir,"fonts/AaFuMengTi-2.ttf"))
+//    )
+}
