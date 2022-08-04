57b44db66e97975ac19f62139b59f1d552bb51fd
mystery0 <mystery0dyl520@gmail.com>
2022-08-04 20:36:49 +0800

    fix: 依赖项更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
-        //如果拉取太慢，请注释掉该行
-        maven("https://nexus3.mystery0.vip/repository/maven-public/")
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
         // NOTE: Do not place your application dependencies here; they belong
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 repositories {
-    maven("https://nexus3.mystery0.vip/repository/maven-public/")
     google()
     mavenCentral()
 }
