818895055a2d946837d403c27157fb45b5649f09
mystery0 <mystery0dyl520@gmail.com>
2022-10-03 22:24:29 +0800

    fix: 更新依赖版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,7 +121,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/jni/CMakeLists.txt")
-            version = "3.18.1"
+            version = "3.22.1"
         }
     }
 }
