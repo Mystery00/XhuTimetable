5c6072ef46bd5c5d69b69b2256efe8c857f830c1
Mystery0 <mystery0dyl520@gmail.com>
2023-05-03 19:38:38 +0800

    ci skip: 迁移通知公告界面

--- app/src/main/java/vip/mystery0/xhu/timetable/repository/NoticeRepo.kt
+++ app/src/main/java/vip/mystery0/xhu/timetable/repository/NoticeRepo.kt
@@ -1,15 +1,4 @@
 package vip.mystery0.xhu.timetable.repository
 
-import vip.mystery0.xhu.timetable.model.entity.Notice
-import vip.mystery0.xhu.timetable.model.response.NoticeResponse
-import vip.mystery0.xhu.timetable.module.Repo
-
-interface NoticeRepo : Repo {
-    suspend fun queryAllNotice(): List<Notice>
-
-    suspend fun hasUnReadNotice(): Boolean
-
-    suspend fun saveList(noticeList: List<NoticeResponse>) {}
-
-    suspend fun markAllAsRead() {}
+object NoticeRepo {
 }
