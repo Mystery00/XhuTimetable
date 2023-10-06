42b5d9c19c81ab4b1dd2a35eea2ecb57525f94c2
Mystery0 <mystery0dyl520@gmail.com>
2023-10-06 16:06:32 +0800

    fix: 更新依赖版本，注意该版本推送坏了

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.huawei.agconnect:agcp:1.9.0.300")
     }
