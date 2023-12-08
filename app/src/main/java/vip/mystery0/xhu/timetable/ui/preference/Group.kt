d7862cfe5f71fa7d96e79f4dfcbd65e908f8320e
Mystery0 <mystery0dyl520@gmail.com>
2023-12-09 00:28:10 +0800

    fix: 设置页面布局调整

--- app/src/main/java/vip/mystery0/xhu/timetable/ui/preference/Group.kt
+++ app/src/main/java/vip/mystery0/xhu/timetable/ui/preference/Group.kt
@@ -1,12 +1,59 @@
 package vip.mystery0.xhu.timetable.ui.preference
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import me.zhanghai.compose.preference.PreferenceCategory
 import me.zhanghai.compose.preference.ProvidePreferenceTheme
+import me.zhanghai.compose.preference.preferenceTheme
+
+@Composable
+fun XhuFoldSettingsGroup(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    foldState: MutableState<Boolean> = remember { mutableStateOf(false) },
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    ProvidePreferenceTheme(
+        theme = xhuPreferenceTheme(),
+    ) {
+        PreferenceCategory(
+            title = title,
+            modifier = Modifier.clickable(
+                indication = null,
+                interactionSource = MutableInteractionSource(),
+            ) {
+                foldState.value = !foldState.value
+            },
+        )
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .animateContentSize(),
+        ) {
+            if (foldState.value) {
+                Spacer(
+                    modifier = Modifier
+                        .height(12.dp),
+                )
+            } else {
+                content()
+            }
+        }
+    }
+}
 
 @Composable
 fun XhuSettingsGroup(
@@ -14,14 +61,20 @@ fun XhuSettingsGroup(
     modifier: Modifier = Modifier,
     content: @Composable ColumnScope.() -> Unit,
 ) {
-    ProvidePreferenceTheme {
+    ProvidePreferenceTheme(
+        theme = xhuPreferenceTheme(),
+    ) {
         PreferenceCategory(
             title = title,
         )
         Column(
             modifier = modifier.fillMaxWidth(),
-        ) {
-            content()
-        }
+            content = content,
+        )
     }
 }
+
+@Composable
+private fun xhuPreferenceTheme() = preferenceTheme(
+    summaryColor = MaterialTheme.colorScheme.outline,
+)
