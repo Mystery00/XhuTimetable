dd452cd94be687fc2f9a69b5bed593b9579cff35
Mystery0 <mystery0dyl520@gmail.com>
2023-12-09 16:44:05 +0800

    fix: 默认主题调整

--- app/src/main/java/vip/mystery0/xhu/timetable/ui/theme/Theme.kt
+++ app/src/main/java/vip/mystery0/xhu/timetable/ui/theme/Theme.kt
@@ -2,17 +2,15 @@ package vip.mystery0.xhu.timetable.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,19 +18,9 @@ import vip.mystery0.xhu.timetable.config.store.GlobalConfigStore
 import vip.mystery0.xhu.timetable.model.entity.NightMode
 import vip.mystery0.xhu.timetable.ui.theme.XhuFonts.globalSet
 
-private val DarkColorPalette = darkColorScheme()
-
-private val LightColorPalette = lightColorScheme(
-    primary = Color(0xFF2196F3),
-    primaryContainer = Color(0xFF1976D2),
-    secondary = Color(0xFF4CAF50),
-    secondaryContainer = Color(0xFF2196F3),
-)
-
 @Composable
 fun isDarkMode(): Boolean {
     val mode by Theme.nightMode.collectAsState()
-//    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     return when (mode) {
         NightMode.AUTO -> isSystemInDarkTheme()
         NightMode.ON -> true
@@ -42,17 +30,48 @@ fun isDarkMode(): Boolean {
 }
 
 @Composable
+private fun getColorScheme(): ColorScheme {
+    val mode by Theme.nightMode.collectAsState()
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    when (mode) {
+        NightMode.MATERIAL_YOU -> {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                //满足Material You条件
+                val context = LocalContext.current
+                if (isSystemInDarkTheme)
+                    dynamicDarkColorScheme(context)
+                else
+                    dynamicLightColorScheme(context)
+            } else {
+                //不满足Material You条件，降级为自动
+                if (isSystemInDarkTheme) {
+                    DarkColorScheme
+                } else {
+                    LightColorScheme
+                }
+            }
+        }
+
+        //强制开启夜间模式
+        NightMode.ON -> return DarkColorScheme
+        //强制关闭夜间模式
+        NightMode.OFF -> return LightColorScheme
+
+        NightMode.AUTO -> {
+            return if (isSystemInDarkTheme) {
+                DarkColorScheme
+            } else {
+                LightColorScheme
+            }
+        }
+    }
+}
+
+@Composable
 fun XhuTimetableTheme(
     content: @Composable () -> Unit
 ) {
-    val isDark = isDarkMode()
-    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-    val colorScheme = if (dynamicColor) {
-        val context = LocalContext.current
-        if (isDark) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-    } else {
-        if (isDark) DarkColorPalette else LightColorPalette
-    }
+    val colorScheme = getColorScheme()
 
     val replacementTypography = MaterialTheme.typography.globalSet(XhuFonts.DEFAULT)
 
