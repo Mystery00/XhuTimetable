5bbcb541921d5c880ff28a56fe48276e133d4b71
Mystery0 <mystery0dyl520@gmail.com>
2024-05-06 22:43:26 +0800

    fix: 更新依赖版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         classpath("com.huawei.agconnect:agcp:1.9.1.301")
     }
 }
 
 plugins {
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.1.0"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "11.1.4"
 }
 
 tasks.register("clean", Delete::class) {
