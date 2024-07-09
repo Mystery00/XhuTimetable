6922378f493395edab069fcad64ab01c9993a32a
Mystery0 <mystery0dyl520@gmail.com>
2024-07-09 20:43:48 +0800

    fix: 更新依赖版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("com.android.tools.build:gradle:8.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         classpath("com.huawei.agconnect:agcp:1.9.1.301")
     }
