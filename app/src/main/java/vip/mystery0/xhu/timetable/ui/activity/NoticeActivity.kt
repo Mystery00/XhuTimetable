d13aab395c9f5ddedaed0cfa08a6c89f871ed996
Mystery0 <mystery0dyl520@gmail.com>
2024-12-29 13:56:59 +0800

    feat: 更新公告详情页面
    - 使用 `AnnotatedString`  `LinkAnnotation`  `SpanStyle` 实现公告内容中的链接和样式。
    - 移除未使用的 `Color` 引用。

--- app/src/main/java/vip/mystery0/xhu/timetable/ui/activity/NoticeActivity.kt
+++ app/src/main/java/vip/mystery0/xhu/timetable/ui/activity/NoticeActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
